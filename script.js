f2c83189b1516613466758855fd451f3f22b3aba
laraib2333 <laraibahmed2333@gamil.com>
2024-10-21 23:23:18 +0500

    first commit

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ function calculate() {
     let maximumAge = parseInt(document.getElementById('Mxm-age').value);
     let amtPerDay = parseInt(document.getElementById('S-p-d').value);
 
-    if (isNaN(currentAge) || isNaN(maximumAge) || isNaN(amtPerDay) || !fritSnack) {
-        document.getElementById('message').innerText = 'Please fill in all fields correctly.';
-        return;
-    }
-
     var total = (maximumAge - currentAge) * amtPerDay * 365;
 
     document.getElementById('message').innerText = 
